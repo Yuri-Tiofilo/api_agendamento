adf3ce1b1f90d8b344eaeab484a8a00f6ffc25b5
Yuri Silva <yusilva12345@gmail.com>
2020-03-22 14:39:43 -0300

    Relationship between files and users

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
+import authMiddleare from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import authMiddleare from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,8 +16,6 @@ routes.use(authMiddleare);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
