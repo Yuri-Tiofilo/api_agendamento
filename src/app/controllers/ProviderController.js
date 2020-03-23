3a53e8d6a74a567a12e626fb14df6c0bbd4cdbaf
Yuri Silva <yusilva12345@gmail.com>
2020-03-23 10:04:07 -0300

    List Of Providers

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,23 @@
+import User from '../models/User';
+import File from '../models/File';
+
+class ProviderController {
+  async index(req, res) {
+    // const providers = await User.findAll(); -> retorna todos os usuários
+    const providers = await User.findAll({
+      where: { provider: true },
+      attributes: ['id', 'name', 'email', 'avatar_id'], // apenas os campos que eu quero
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
+      ], // inclui o model File a resposta
+    }); // retorna apenas usuários que são providers
+
+    return res.json(providers);
+  }
+}
+
+export default new ProviderController();
