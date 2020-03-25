d993fbf4c6c2ef75d065db15bad7c0f51fd8ef89
Yuri Silva <yusilva12345@gmail.com>
2020-03-25 17:05:57 -0300

    Adding Sentry and .env in Application

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '8bc6b4709ea79a01efc18dc070d478de',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'd775e4543d2d77',
-    pass: 'd931b0027c9605',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Nuggets 24h <noreply@equipenuggets.com>',
