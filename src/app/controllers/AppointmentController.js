8c0f5875eda8d16449eeeaf418881a838b279646
Yuri Silva <yusilva12345@gmail.com>
2020-03-25 12:05:34 -0300

    Template and Sending of email to provider

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async index(req, res) {
@@ -59,9 +60,16 @@ class AppointmentController {
         provider: true,
       },
     });
+
     if (!isProvider) {
       return res.status(401).json({ error: 'Provider does not exist' });
     }
+
+    // if (provider_id === req.userId) {
+    //   return res
+    //     .status(401)
+    //     .json({ error: 'A provider cannot schedule for himself' });
+    // }
 
     const hourStart = startOfHour(parseISO(date)); // parseISO -> transforms into a date object
     // startOfHour -> takes only the beginning of the hour, for example 19:30 it takes 19 only.
@@ -70,6 +78,7 @@ class AppointmentController {
       // isBefore -> it compares whether the hourStart date is equal to new Date();
       return res.status(400).json({ error: 'Past dates are not permitted' });
     }
+
     const checkAvailability = await Appointment.findOne({
       where: {
         provider_id,
@@ -103,6 +112,62 @@ class AppointmentController {
     await Notification.create({
       content: `Novo agendamento de ${user.name} para${formattedDate}`,
       user: provider_id,
+    });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
+    // const checkUserProvider = await User.findOne({
+    //   where: { id: req.userId, provider: true },
+    // });
+    // if (checkUserProvider) {
+    //   return res.status(401).json({ error: 'User is a provider.' });
+    // }
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments 2 hours in advance',
+      });
+    }
+
+    appointment.canceled = new Date();
+
+    await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "' dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd775e4543d2d77',
+    pass: 'd931b0027c9605',
+  },
+  default: {
+    from: 'Nuggets 24h <noreply@equipenuggets.com>',
+  },
+};
