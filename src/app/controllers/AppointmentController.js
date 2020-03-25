d4edeb02719d597566c2bd18bb08cde353f4e85a
Yuri Silva <yusilva12345@gmail.com>
2020-03-25 16:14:10 -0300

    Adding reis, queue, and time listing for client

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,7 +5,8 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancelationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -19,7 +20,7 @@ class AppointmentController {
       order: ['date'],
       limit: perPage, // limit paginate
       offset: (page - 1) * perPage, // how many will skip paging
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       include: [
         {
           model: User,
@@ -132,18 +133,18 @@ class AppointmentController {
         },
       ],
     });
-    // const checkUserProvider = await User.findOne({
-    //   where: { id: req.userId, provider: true },
-    // });
-    // if (checkUserProvider) {
-    //   return res.status(401).json({ error: 'User is a provider.' });
-    // }
-
-    if (appointment.user_id !== req.userId) {
-      return res.status(401).json({
-        error: "You don't have permission to cancel this appointment",
-      });
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+    if (checkUserProvider) {
+      return res.status(401).json({ error: 'User is a provider.' });
     }
+
+    // if (appointment.user_id !== req.userId) {
+    //   return res.status(401).json({
+    //     error: "You don't have permission to cancel this appointment",
+    //   });
+    // }
 
     const dateWithSub = subHours(appointment.date, 2);
 
@@ -157,17 +158,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "' dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    await Queue.add(CancelationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
